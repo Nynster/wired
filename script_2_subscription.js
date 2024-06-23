577fa1bf9471e8eab339dc680665c7bb386a090e
Nynster <aelwen.nynster@gmail.com>
2024-06-23 12:57:16 +0200

    En stand by pour le moment

--- script_2_subscription.js
+++ script_2_subscription.js
@@ -1,14 +1,12 @@
-var pass;
-var  cf_pass  
-     function change()
-     {
-    
-     pass = document.getElementById('password_one_form_subscription').value;
-     cf_pass = document.getElementById('password_two_form_subscription').value;
-	 
-	  if (pass != cf_pass) {
-	  
-     document.getElementById('message_form').innerHTML = "X";
-	 }
+function checkPassword(){
+     let pass = document.getElementById('password_one_form_subscription').value;
+     let conPass = document.getElementById('password_two_form_subscription').value;
+     let message = document.getElementById('message_form');
 
-   };
+     if(pass !== conPass){
+          message.textContent = "Les mots de passe ne sont pas identiques";
+          return false;
+     }else{
+          return true;
+     }
+    }
